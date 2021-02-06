57f70ae0528c027d8f4741381fa2c91cf7f5b902
shivesh-p <shiveshp530@gmail.com>
2021-02-06 13:16:55 +0530

    added scrollable search bar

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React, { Component } from "react";
 import CardList from "./CardList";
 import SearchComponent from "./SearchComponent";
-import './App.css'
+import "./App.css";
+import Scroll from './Scroll'
 
 class App extends Component {
   constructor() {
@@ -14,29 +15,30 @@ class App extends Component {
 
   onSearchChange = (event) => {
     this.setState({ searchField: event.target.value });
-
   };
   componentDidMount() {
-      fetch('https://jsonplaceholder.typicode.com/users')
-      .then(response=>{
-          return response.json();
-        })
-        .then(users=>{
-            this.setState({robots:users})
-        })
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => {
+        return response.json();
+      })
+      .then((users) => {
+        this.setState({ robots: users });
+      });
   }
-  
+
   render() {
     const filteredRobots = this.state.robots.filter((robot) => {
-        return robot.name
-          .toLowerCase()
-          .includes(this.state.searchField.toLowerCase());
-      });
+      return robot.name
+        .toLowerCase()
+        .includes(this.state.searchField.toLowerCase());
+    });
     return (
       <div className="tc">
         <h1 className="f1">RoboFriends</h1>
         <SearchComponent searchChange={this.onSearchChange} />
-        <CardList robots={filteredRobots} />
+        <Scroll>
+          <CardList robots={filteredRobots} />
+        </Scroll>
       </div>
     );
   }
